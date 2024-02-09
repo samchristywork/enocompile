a6bdb54fafadd94d8438c3aed09b614c6fbd033c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-02-09 17:06:34 -0600

    feat: Add server middleware
    This middleware will print out a nice colored output with the method,
    route, params, status, etc...

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,41 @@ use chrono::Utc;
 use std::future::Future;
 use tide::{http::mime, utils::After, Next, Request, Response, Result, Server, StatusCode};
 
+fn log_middleware<'a>(
+    req: Request<()>,
+    next: Next<'a, ()>,
+) -> Pin<Box<dyn Future<Output = Result> + Send + 'a>> {
+    Box::pin(async move {
+        let method = req.method();
+        let route = req.url().path().to_string();
+        let params = req.url().query().unwrap_or("").to_string();
+        let res = next.run(req).await;
+        let status = res.status();
+        let date = Utc::now().format("%Y-%m-%d %H:%M:%S");
+
+        match status.is_success() {
+            true => {
+                println!(
+                    "{} {} \x1b[32m{}\x1b[0m {} \x1b[90m[{}]\x1b[0m",
+                    date, method, status, route, params
+                );
+            }
+            false => {
+                println!(
+                    "{} {} \x1b[31m{}\x1b[0m {} \x1b[90m[{}]\x1b[0m",
+                    date, method, status, route, params
+                );
+            }
+        }
+        Ok(res)
+    })
+}
+
 fn new_app() -> Server<()> {
     let mut app = tide::new();
+
+    app.with(log_middleware);
+
     app
 }
 
