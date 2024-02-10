0cffc1d5b6903b714d66b5c5b1f6093e92c6d115
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-02-09 21:31:12 -0600

    feat: Add error handling
    This middleware handles various types of server errors.
    
    I have also changed the main routes a bit.

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,39 @@ fn log_middleware<'a>(
     })
 }
 
+const NOT_FOUND_HTML_PAGE: &str = "<html><body>
+  <h1>File Not Found</h1>
+  <p>Error 404: The requested file was not found on this server.</p>
+</body></html>";
+
+const INTERNAL_SERVER_ERROR_HTML_PAGE: &str = "<html><body>
+  <h1>Internal Server Error</h1>
+  <p>Error 500: The server encountered an internal error and was unable to complete your request.</p>
+</body></html>";
+
 fn new_app() -> Server<()> {
     let mut app = tide::new();
+
+    app.with(After(|response: Response| async move {
+        Ok(match response.status() {
+            StatusCode::InternalServerError => Response::builder(500)
+                .content_type(mime::HTML)
+                .body(INTERNAL_SERVER_ERROR_HTML_PAGE)
+                .build(),
+
+            _ => response,
+        })
+    }));
+
+    app.with(After(|response: Response| async move {
+        Ok(match response.status() {
+            StatusCode::NotFound => Response::builder(404)
+                .content_type(mime::HTML)
+                .body(NOT_FOUND_HTML_PAGE)
+                .build(),
+            _ => response,
+        })
+    }));
 
     app.with(log_middleware);
 
@@ -45,9 +76,8 @@ fn new_app() -> Server<()> {
 async fn main() -> Result<()> {
     let mut app = new_app();
 
-    app.at("/").serve_file("static/index.html")?;
-    app.at("/").serve_dir("static/")?;
-    app.at("/generated").serve_dir("generated/")?;
+    app.at("/").serve_file("generated/index.html")?;
+    app.at("/").serve_dir("generated/")?;
     app.at("/images").serve_dir("images/")?;
     app.at("/articles").serve_dir("articles/")?;
 
